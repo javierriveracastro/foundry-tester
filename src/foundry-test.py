e53aa9abce1a13f2324dffcfd0dfc9eca1cf3eaf
javier <javier@castroparga.com>
2022-11-17 16:18:16 +0100

    Opening the driver

--- src/foundry-test.py
+++ src/foundry-test.py
@@ -6,7 +6,34 @@ import os
 import time
 import subprocess
 
+from selenium import webdriver
+
 from settings import FOUNDRY_BIN_DIR, FOUNDRY_DATA_DIR
+
+
+def close_test(open_driver, open_foundry_proc):
+    """
+    Closes the webdriver and the Foundry instance as clean as possible
+    """
+    open_driver.quit()
+    open_foundry_proc.terminate()
+    try:
+        open_foundry_proc.wait(20)
+    except subprocess.TimeoutExpired:
+        open_foundry_proc.kill()
+    print("Foundry stopped")
+
+
+def start_webdriver():
+    """
+    Create the webdriver instance and point it to foundry
+    :return: a webdriver instance
+    """
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:30000")
+    time.sleep(10)
+    return driver
+
 
 if __name__ == '__main__':
     os.chdir(FOUNDRY_BIN_DIR)
@@ -15,9 +42,5 @@ if __name__ == '__main__':
         ['node', 'resources/app/main.js', f'--dataPath={FOUNDRY_DATA_DIR}'])
     time.sleep(10)
     print("Foundry Running")
-    foundry_process.terminate()
-    try:
-        foundry_process.wait(20)
-    except subprocess.TimeoutExpired:
-        foundry_process.kill()
-    print("Foundry stopped")
+    web_driver = start_webdriver()
+    close_test(web_driver, foundry_process)
